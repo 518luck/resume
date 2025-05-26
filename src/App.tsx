b0bd53c30da7d74e14de6881a4108d3e1a7f2151
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-05-26 20:16:07 +0800

    个人信息结构搭建

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+
 import styles from '@/styles/App.module.scss'
+
+// 测试链接
+import AccountInfoModal from '@/components/AccountInfoModal'
 
 function App() {
   return (
@@ -8,6 +12,17 @@ function App() {
       <Header />
       <main>主内容区</main>
       <Footer />
+      <hr
+        style={{
+          width: '100%',
+          height: '1px',
+          border: 'none',
+          background: 'red',
+        }}
+      />
+      实时展示区
+      <br />
+      <AccountInfoModal />
     </div>
   )
 }
--- src/components/AccountInfoModal/index.tsx
+++ src/components/AccountInfoModal/index.tsx
@@ -1,3 +1,58 @@
-const AccountInfoModal = () => {}
+import style from './AccountInfoModal.module.scss'
+
+import yanjing from '@/assets/test/眼镜.jpeg'
+
+const AccountInfoModal = () => {
+  return (
+    <div className={style.wrap}>
+      <div className={style.nav}>
+        <div className={style.btn}>
+          <div className={style.btnItem}></div>
+          <div className={style.btnItem}></div>
+          <div className={style.btnItem}></div>
+        </div>
+        <div className={style.icon}>
+          <div className={style.iconImg}>
+            <img src={yanjing} alt='' />
+          </div>
+          <div className={style.iconCon}>
+            <p>不吃香菜</p>
+            <h2>备注</h2>
+          </div>
+          <div className={style.line}></div>
+          <div className={style.title}>
+            <p>标题</p>
+          </div>
+          <div className={style.menu}>
+            <div className={style.item}>
+              <div className={style.light}></div>
+              <div className={style.licon}>
+                <span
+                  className={`${style.iconfont} ${style.iconWenjian1}`}></span>
+              </div>
+              <div className={style.con}>Payouts</div>
+              <div className={style.ricon}></div>
+            </div>
+          </div>
+          <div className={style.line}></div>
+          <div className={style.title}>
+            <p>Sever 6</p>
+          </div>
+          <div className={style.server}>
+            <div className={style.item}>
+              <div className={style.light}></div>
+              <div className={style.licon}>
+                <span
+                  className={`${style.iconfont} ${style.iconWenjian1}`}></span>
+              </div>
+              <div className={style.con}>Payouts</div>
+              <div className={style.ricon}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default AccountInfoModal
