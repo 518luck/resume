04a53baf548764e47759ac852fd640c55b4ce212
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-05-27 17:45:36 +0800

    头部中间位置的基础内容

---
+++ src/layouts/Header/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import style from './Navbar.module.scss'
+
+export const Navbar = () => {
+  return (
+    <div className={style.content}>
+      中间占位符号
+      <span>壁纸标签</span>
+      <span>电脑壁纸</span>
+      <span>手机壁纸</span>
+      <span>头像制作</span>
+    </div>
+  )
+}
--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import style from './Header.module.scss'
 import AccountInfoModal from '@/components/AccountInfoModal'
 
+import { Navbar } from './components/Navbar'
+
 const Header = () => {
   const [showModal, setShowModal] = useState<boolean>(false)
 
@@ -10,7 +12,9 @@ const Header = () => {
   return (
     <div className={style.container}>
       <div className={style.leftSection}>左侧模块,预计宣言</div>
-      <div className={style.centerSection}>中间模块</div>
+      <div className={style.centerSection}>
+        <Navbar />
+      </div>
       <div className={style.rightSection}>
         右侧模块
         <button onClick={() => setShowModal(!showModal)}>头像</button>
