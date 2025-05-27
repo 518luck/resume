1dc6daa07bc7f78f97d775a2612d792a0c7e0912
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-05-27 22:54:36 +0800

    主页导航界面的

--- src/layouts/Header/components/Navbar/index.tsx
+++ src/layouts/Header/components/Navbar/index.tsx
@@ -1,13 +1,28 @@
+import { UsbOutlined } from '@ant-design/icons'
+
 import style from './Navbar.module.scss'
 
 export const Navbar = () => {
   return (
     <div className={style.content}>
-      中间占位符号
-      <span>壁纸标签</span>
-      <span>电脑壁纸</span>
-      <span>手机壁纸</span>
-      <span>头像制作</span>
+      <div className={style.TopBody}>
+        <div className={style.navItem}>
+          <UsbOutlined />
+          <p>壁纸标签</p>
+        </div>
+        <div className={style.navItem}>
+          <UsbOutlined />
+          <p>电脑壁纸</p>
+        </div>
+        <div className={style.navItem}>
+          <UsbOutlined />
+          <p>手机壁纸</p>
+        </div>
+        <div className={style.navItem}>
+          <UsbOutlined />
+          <p>头像制作</p>
+        </div>
+      </div>
     </div>
   )
 }
